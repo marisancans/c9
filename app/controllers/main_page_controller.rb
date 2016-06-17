a88a8ad0ff755e0c7d661ded195ea05e6ebebf47
xcello <marisancans@gmail.com>
2016-06-17 13:00:53 +0000

    Previous questions on finish page commented

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -4,7 +4,7 @@ class MainPageController < ApplicationController
         @categories = Question.uniq.pluck(:category)
         Visiter.create(ip: request.remote_ip, total_question_count: 0) if !Visiter.exists?(ip: request.remote_ip)
         session[:previous_question_ids] = []
-        session[:previous_question_correct] = []
+        session[:previous_question_selected] = []
         session[:random] = false
     end
     
